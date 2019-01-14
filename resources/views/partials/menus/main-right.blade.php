98bbd61ec4a7f4e687ec29c9c478d69b6542818b
Alfredo Fernandez <afernandez8781@hotmail.com>
2019-01-14 00:55:10 -0400

    Part 14 User Authentication

---
+++ resources/views/partials/menus/main-right.blade.php
@@ -0,0 +1,37 @@
+<ul>
+    @guest
+    <li><a href="{{ route('register') }}">Regístrate</a></li>
+    <li><a href="{{ route('login') }}">Inisiar Sesión</a></li>
+    @else
+    <li>
+        <a href="{{ route('logout') }}"
+            onclick="event.preventDefault();
+                     document.getElementById('logout-form').submit();">
+            Cerrar Sesión
+        </a>
+    </li>
+
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            {{ csrf_field() }}
+        </form>
+    @endguest
+
+    <li><a href="{{ route('cart.index') }}">Carrito
+    @if (Cart::instance('default')->count() > 0 )
+        <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>
+    @endif
+</a></li>
+    
+ {{--    @foreach($items as $menu_item)
+        <li>
+            <a href="{{ $menu_item->link() }}">
+                {{ $menu_item->title }}
+                @if ($menu_item->title === 'Cart')
+                    @if (Cart::instance('default')->count() > 0)
+                    <span class="cart-count"><span>{{ Cart::instance('default')->count() }}</span></span>
+                    @endif
+                @endif
+            </a>
+        </li>
+    @endforeach --}}
+</ul>
